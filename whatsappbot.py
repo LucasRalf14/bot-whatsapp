5db04b0589360269346945a30563b54f4908c42a
LucasRalf14 <lucas_w.o.ralf@hotmail.com>
2020-11-25 21:06:21 -0300

    Código finalizado (com busca de contato e envio de mensagem)

--- whatsappbot.py
+++ whatsappbot.py
@@ -2,12 +2,34 @@
 from selenium import webdriver
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
 #navegar até o wpp web
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://web.whatsapp.com/')
-time.sleep(30)
-#definir contatos, grupos e mensagen a ser enviada
-contatos = 'Wserver'
+time.sleep(15)
+
+#definir contatos, grupos e mensagem a ser enviada
+contatos = ['Wserver', 'Gabriela']
+mensagem = 'olá!'
 
 #buscar contatos/grupos
+def buscar_contato(contato):
+    campo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+    time.sleep(3)
+    campo_pesquisa.click()
+    campo_pesquisa.send_keys(contato)
+    campo_pesquisa.send_keys(Keys.ENTER)
+
 #enviar mensagens para o contato/grupo
+def enviar_mensagem(mensagem):
+    campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+    campo_mensagem[1].click()
+    time.sleep(3)
+    campo_mensagem[1].send_keys(mensagem)
+    campo_mensagem[1].send_keys(Keys.ENTER)
+
+#pesquisa no array de contatos
+for contato in contatos:
+    buscar_contato(contato)
+    enviar_mensagem(mensagem)
